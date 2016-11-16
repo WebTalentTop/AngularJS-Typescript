269268ef1bfbaafafc22d0da9b94083444e50206
Kalyan <kalyan@12thwonder.com>
2016-11-15 19:03:34 -0800

    Titan User Grid Changes

--- Titan.WebCore/app/shared/services/apiUrlConst/titanUserApiUrls.ts
+++ Titan.WebCore/app/shared/services/apiUrlConst/titanUserApiUrls.ts
@@ -1,7 +1,7 @@
 ﻿import { titanApiUrl } from './titanApiUrl';
 import { GridApiUrl } from './gridApiUrls';
 export class TitanUserApiUrl {
-    public static gridApiUrl:string  = GridApiUrl.testFacilityGridUrl;
+    public static gridApiUrl:string  = GridApiUrl.titanUserGridUrl;
     public static postCreatedUrl: string = titanApiUrl + 'titanUser';
     public static postUpdateUrl: string = titanApiUrl + 'titanUser';
     public static getByIdUrl: string = titanApiUrl + 'titanUser';
