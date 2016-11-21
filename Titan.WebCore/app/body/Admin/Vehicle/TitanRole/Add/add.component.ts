bbe54506cd8f6d06c147daa498eb811a4e76d99c
Kalyan <kalyan@12thwonder.com>
2016-11-20 16:34:51 -0800

    Removed duplicate footer and styles.

--- Titan.WebCore/app/body/Admin/Vehicle/TitanRole/Add/add.component.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/TitanRole/Add/add.component.ts
@@ -5,16 +5,14 @@ import { SelectItem } from 'primeng/primeng';
 //import { DataTable,PanelMenuModule, PanelModule ,InputTextModule,InputTextareaModule, ButtonModule } from 'primeng/primeng';
 
 @Component({
-    selector: 'add-grade',
-    styleUrls: ['app/body/Admin/Vehicle/Grade/Add/add.component.css'], 
-    templateUrl: 'app/body/Admin/Vehicle/Grade/Add/add.component.html'
+    selector: 'add-titanrole',
+    styleUrls: ['app/body/Admin/Vehicle/TitanRole/Add/add.component.css'], 
+    templateUrl: 'app/body/Admin/Vehicle/TitanRole/Add/add.component.html'
 })
 
 export class AddComponent {
     username: string;
     description:string;
-    //constructor(private dataService: service) {
-    //        }
 
     constructor(private service: TitanRoleService) {
 
