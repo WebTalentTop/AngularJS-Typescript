7b63efedf66e0f8c4ddc6f53893f5d19c15bc853
Kalyan <kalyan@12thwonder.com>
2016-11-28 15:25:00 -0800

    Removed css in vehicle page

--- Titan.WebCore/app/body/Admin/Vehicle/vehicle.component.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/vehicle.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'vehicle',
-    styleUrls: ['app/body/Admin/Vehicle/vehicle.component.css'],
     templateUrl: 'app/body/Admin/Vehicle/vehicle.component.html'
 })
 export class VehicleComponent {
