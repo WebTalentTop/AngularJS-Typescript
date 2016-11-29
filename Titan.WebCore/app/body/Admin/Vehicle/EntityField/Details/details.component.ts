f436ec713d3d747e08c38c56832125eaeece1bf7
Kalyan <kalyan@12thwonder.com>
2016-11-29 10:26:45 -0800

    EntityField Add and details page

---
+++ Titan.WebCore/app/body/Admin/Vehicle/EntityField/Details/details.component.ts
@@ -0,0 +1,85 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { EntityFieldService } from '../../../../../shared/services/entityField.service'
+import { DataTable, TabViewModule, LazyLoadEvent, ButtonModule, InputTextareaModule, MessagesModule, InputTextModule, PanelModule, FileUploadModule, Message, GrowlModule } from 'primeng/primeng';
+import { SelectItem, ConfirmationService } from 'primeng/primeng';
+import { Validators } from '@angular/forms';
+
+@Component({
+    selector: 'entityField-detail',
+    templateUrl: 'app/body/Admin/Vehicle/EntityField/Details/details.component.html'
+})
+export class DetailsComponent {
+    username: string;
+    details: string;
+
+    id: string;
+    entityType: string = "EntityField";
+    entityId: string = this.id;
+    filepath: string = "EntityField";
+    entityField = { name: '' };
+    selectedUserNames: Array<any> = new Array();
+    filteredUserNames: Array<any> = new Array();
+    filteredSelectedUserNames: Array<any> = new Array();
+    formConfiguration: any;
+    formObject: any;
+
+    model: any = {
+        id: '',
+        isDeleted: false,
+        IsRequired:'',
+        CreatedOn:'',
+        AllowMultiSelect:'',
+        description: ''
+    };
+
+
+    msgs: Message[];
+    uploadedFiles: any[] = [];
+
+
+    public EntityFieldDetails: any;
+    public EntityFieldId: string;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private service: EntityFieldService,
+    )
+    { }
+
+
+    ngOnInit() {
+        this.route.params.forEach((params: Params) => {
+            this.route.params.subscribe(params => console.log(params['id']));
+
+            this.EntityFieldId = params['id']; // (+) converts string 'id' to a number
+            //let locale = params['locale'];
+
+            this.service.getById(this.EntityFieldId).subscribe(EntityFieldDetails => {
+                this.EntityFieldDetails = EntityFieldDetails;
+                this.EntityFieldDetails.id = this.EntityFieldId;
+                console.log(this.EntityFieldDetails);
+            });
+        });
+    }
+
+
+    onSubmit(formRef) {
+        console.log("inside");
+        console.log(this.entityField.name);
+        formRef.isDeleted = false;
+        let formData: any = {
+            id: this.id,
+            name: '',
+        };
+        
+        formData.id = this.id;
+        formData.name = formRef.name;
+        
+        this.service.postUpdate(this.EntityFieldDetails).subscribe(EntityFieldDetails => {
+            console.log(EntityFieldDetails);
+        });
+    }
+}
---
+++ Titan.WebCore/app/body/Admin/Vehicle/EntityField/Details/details.module.ts
@@ -0,0 +1,19 @@
+﻿import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from '@angular/forms';
+import { DetailsComponent } from "./details.component";
+import { EntityFieldService} from '../../../../../shared/services/entityField.service';
+
+import { DataTableModule, ButtonModule, InputTextareaModule, InputTextModule, PanelModule, GrowlModule, DropdownModule, CalendarModule } from 'primeng/primeng';
+import { RouterModule } from "@angular/router";
+import detailsRoutes from "./details.routes";
+
+@NgModule({
+    imports: [CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, 
+        ButtonModule, DropdownModule, GrowlModule CalendarModule, detailsRoutes],
+    providers: [EntityFieldService],
+    declarations: [DetailsComponent],
+    exports: [DetailsComponent]
+})
+
+export default class DetailsModule { }
