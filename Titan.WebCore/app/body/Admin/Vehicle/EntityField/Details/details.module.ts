fc8109f6736a00c88e87968409714bee03c30329
Archana <archana@12thwonder.com>
2016-11-29 16:34:49 -0800

    messages

--- Titan.WebCore/app/body/Admin/Vehicle/EntityField/Details/details.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/EntityField/Details/details.module.ts
@@ -9,8 +9,13 @@ import { RouterModule } from "@angular/router";
 import detailsRoutes from "./details.routes";
 
 @NgModule({
+<<<<<<< b0d3f37a7c375a06f98f5d2c218c9ba63ea2e094
     imports: [CommonModule, FormsModule, DataTableModule,GrowlModule ,InputTextareaModule, InputTextModule, PanelModule, 
         ButtonModule, DropdownModule, GrowlModule CalendarModule, detailsRoutes],
+=======
+    imports: [CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, 
+        ButtonModule, DropdownModule, GrowlModule, CalendarModule, detailsRoutes],
+>>>>>>> 264e62ea6df64c2b397d7e76dd58c7e510045f54
     providers: [EntityFieldService],
     declarations: [DetailsComponent],
     exports: [DetailsComponent]
