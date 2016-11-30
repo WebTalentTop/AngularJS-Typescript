54c52a498e971d12b1c463c41a0a5b0dc43aa186
Kalyan <kalyan@12thwonder.com>
2016-11-30 12:01:48 -0800

    Market Validations

--- Titan.WebCore/app/body/Admin/Vehicle/Market/market.routes.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/Market/market.routes.ts
@@ -2,6 +2,7 @@ import { MarketComponent } from './market.component';
 import { RouterModule } from "@angular/router";
 
 const routes = [
+    { path: '/:page', component: MarketComponent },
     { path: '', component: MarketComponent },
     { path: 'add', loadChildren: 'app/body/Admin/Vehicle/Market/Add/add.module' },
     { path: 'details/:id', loadChildren: 'app/body/Admin/Vehicle/Market/Details/details.module'}
