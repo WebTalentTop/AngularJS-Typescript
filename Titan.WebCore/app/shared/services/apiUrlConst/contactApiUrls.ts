cbb8a22cd31c3d1fe9ae4ff823771ee638478e10
Archana <archana@12thwonder.com>
2016-12-01 13:39:37 -0800

    contact

--- Titan.WebCore/app/shared/services/apiUrlConst/contactApiUrls.ts
+++ Titan.WebCore/app/shared/services/apiUrlConst/contactApiUrls.ts
@@ -2,7 +2,7 @@ import { titanApiUrl } from './titanApiUrl';
 import { GridApiUrl } from './gridApiUrls';
 
 export class ContactApiUrl {
-    public static gridApiUrl:string  = GridApiUrl.contactridUrl;
+    public static gridApiUrl: string = GridApiUrl.contactGridUrl;
     public static postCreatedUrl: string = titanApiUrl + 'contact/post';
     public static postUpdateUrl: string = titanApiUrl + 'contact/put';
     public static getByIdUrl: string = titanApiUrl + 'contact/get';
