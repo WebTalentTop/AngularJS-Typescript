2d85f9f5a23844007cfe95f3f52f0c74c9e65ecc
Kalyan <kalyan@12thwonder.com>
2016-11-30 18:21:44 -0800

    Grid changes

--- Titan.WebCore/app/shared/services/apiUrlConst/testStatusApiUrls.ts
+++ Titan.WebCore/app/shared/services/apiUrlConst/testStatusApiUrls.ts
@@ -3,7 +3,7 @@ import { GridApiUrl }  from './gridApiUrls';
 
 export class testStatusApiUrl {
     public static gridApiUrl:string  = GridApiUrl.testStatusGridUrl;
-    public static postCreatedUrl: string = titanApiUrl + 'testStatus';
-    public static postUpdateUrl: string = titanApiUrl + 'testStatus';
-    public static getByIdUrl: string = titanApiUrl + 'testStatus';
+    public static postCreatedUrl: string = titanApiUrl + 'testStatus/post';
+    public static postUpdateUrl: string = titanApiUrl + 'testStatus/put';
+    public static getByIdUrl: string = titanApiUrl + 'testStatus/get';
 }
