3bf2b269612c66f89b103855db2a7afe137d02ba
Kalyan <kalyan@12thwonder.com>
2016-12-07 20:37:56 -0800

    Stylings and changes in naming

--- Titan.WebCore/app/body/Admin/Vehicle/TitanUser/Add/add.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/TitanUser/Add/add.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from '@angular/forms';
 import { AddComponent } from "./add.component";
-import { TitanUserService} from '../../../../../shared/services/titanuser.service';
+import { TitanUserService} from '../../../../../shared/services/titanUser.service';
 
 import { DataTableModule, ButtonModule, InputTextareaModule,InputTextModule, PanelModule, DropdownModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
