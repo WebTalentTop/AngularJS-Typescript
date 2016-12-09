904d1b8f2833972dfe5f7729c6776e7248ea15d6
Brian Sexton <brian@12thwonder.com>
2016-12-09 11:52:58 -0800

    Remove some cached resource and build files so Git can start properly ignoring them.

--- Titan.WebCore/wwwroot/EnvironmentApiUrl/localoption1/titanApiUrl.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.titanApiUrl = "http://localhost:9998/api/";
-//# sourceMappingURL=titanApiUrl.js.map
--- Titan.WebCore/wwwroot/EnvironmentApiUrl/localoption2/titanApiUrl.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.titanApiUrl = "http://titanapi-local.12thwonder.com/api/";
-//# sourceMappingURL=titanApiUrl.js.map
