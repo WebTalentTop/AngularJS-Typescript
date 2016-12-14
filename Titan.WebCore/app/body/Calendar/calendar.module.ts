46ff79b6b94915312be7e766b366a5a3ff497fdf
Archana <archana@12thwonder.com>
2016-12-14 08:53:18 -0800

    filters on calendar

--- Titan.WebCore/app/body/Calendar/calendar.module.ts
+++ Titan.WebCore/app/body/Calendar/calendar.module.ts
@@ -1,20 +1,28 @@
 ﻿import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CalendarComponent } from "./calendar.component";
-import { RouterModule } from "@angular/router";
-import calendarRoutes from "./calendar.routes";
-import { MultiSelectModule } from 'primeng/primeng';
 import { FormsModule } from '@angular/forms';
-import {
-    DataTableModule, AutoCompleteModule, TabViewModule, ButtonModule,
-    InputTextareaModule, InputTextModule, PanelModule, DropdownModule, FileUploadModule, GrowlModule
-} from 'primeng/primeng';
+import { TestFacilityService } from '../../shared/services/testFacility.service';
+
+import { BuildLevelService } from '../../shared/services/buildlevel.service';
+import { TestStatusService } from '../../shared/services/teststatus.service';
+import { TestRoleService } from '../../shared/services/testRole.service';
+import { ProjectService } from '../../shared/services/project.service';
+import { TestModeService } from '../../shared/services/testMode.service';
+import { TestTypeService } from '../../shared/services/testType.service';
+import { DataTableModule, AutoCompleteModule, TabViewModule, ButtonModule,  InputTextareaModule, InputTextModule, PanelModule, DropdownModule, FileUploadModule, GrowlModule } from 'primeng/primeng';
+import { RouterModule } from "@angular/router";
+import { MultiSelectModule } from 'primeng/primeng';
+import calendarRoutes from "./calendar.routes";
+
 
 @NgModule({
     imports: [CommonModule, calendarRoutes, RouterModule, AutoCompleteModule,
         MultiSelectModule, FormsModule, DataTableModule, TabViewModule, ButtonModule,
         InputTextareaModule, DropdownModule, InputTextModule, PanelModule, FileUploadModule, GrowlModule],
-
+    providers: [TestFacilityService, BuildLevelService, ProjectService, TestRoleService, TestStatusService, TestModeService,
+        TestTypeService
+    ],
     declarations: [CalendarComponent]
 })
 
