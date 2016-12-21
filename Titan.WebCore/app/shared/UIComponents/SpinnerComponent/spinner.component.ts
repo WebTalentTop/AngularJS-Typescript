0904aef8eca4fd965cbc6eee97a7e815e17ce8ad
ZeroInfinity12th <atilla@12thWonder.com>
2016-12-21 14:51:08 -0800

    Renaming spinner Component not to clash with Primeng spinner

--- Titan.WebCore/app/shared/UIComponents/SpinnerComponent/spinner.component.ts
+++ Titan.WebCore/app/shared/UIComponents/SpinnerComponent/spinner.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'spinner',
+    selector: 'titan-spinner',
     templateUrl: 'app/shared/UIComponents/SpinnerComponent/spinner.component.html',
     styleUrls:['app/shared/UIComponents/SpinnerComponent/spinner.component.css']
 })
-export class SpinnerComponent {}
+export class TitanSpinnerComponent {}
