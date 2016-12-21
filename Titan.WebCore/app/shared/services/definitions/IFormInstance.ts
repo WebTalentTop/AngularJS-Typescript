3bf6757f3d5aa13c4090001604f3c152da2ef481
ZeroInfinity12th <atilla@12thWonder.com>
2016-12-21 15:28:30 -0800

    FormInstance Interfaces

--- Titan.WebCore/app/shared/services/definitions/IFormInstance.ts
+++ Titan.WebCore/app/shared/services/definitions/IFormInstance.ts
@@ -1,3 +1,23 @@
+import { IFormInstanceFieldDataItem} from "./IFormInstanceFieldDataItem";
 /**
  * Created by ZeroInfinity on 12/20/2016.
  */
+export interface IFormInstance {
+ formSchemaVersionId:string;
+ entityIdentifierId:string;
+ entityId:string;
+ formInstanceStateId?:string;
+ fieldData:IFormInstanceFieldDataItem[];
+}
+
+
+export class FormInstance implements IFormInstance {
+ constructor (
+     public entityId:string,
+     public entityIdentifierId:string,
+     public formSchemaVersionId:string,
+     public fieldData:IFormInstanceFieldDataItem[],
+     public formInstanceStateId?:string
+ )
+ {}
+}
