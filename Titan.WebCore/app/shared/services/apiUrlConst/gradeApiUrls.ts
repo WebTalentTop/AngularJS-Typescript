29958c091b941a5c0ea546430b1dd70d1259685d
ZeroInfinity12th <atilla@12thWonder.com>
2016-12-22 09:41:30 -0800

    GradeApiUrls Missing

--- Titan.WebCore/app/shared/services/apiUrlConst/gradeApiUrls.ts
+++ Titan.WebCore/app/shared/services/apiUrlConst/gradeApiUrls.ts
@@ -1,3 +1,12 @@
 /**
  * Created by ZeroInfinity on 12/20/2016.
  */
+import { titanApiUrl } from './titanApiUrl';
+import { GridApiUrl } from './gridApiUrls';
+
+export class GradeApiUrl {
+    public static gridApiUrl:string  = GridApiUrl.gradeGridUrl;
+    public static postCreatedUrl: string = titanApiUrl + 'grade/post';
+    public static postUpdateUrl: string = titanApiUrl + 'grade/put';
+    public static getByIdUrl: string = titanApiUrl + 'grade/get';
+}
