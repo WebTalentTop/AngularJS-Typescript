b5afdec409a57a407aace6ceca3b8e80883d32ac
Archana <archana@12thwonder.com>
2016-12-27 13:59:27 -0800

    test request add

--- Titan.WebCore/app/body/TestRequest/testrequest.routes.ts
+++ Titan.WebCore/app/body/TestRequest/testrequest.routes.ts
@@ -3,7 +3,7 @@ import { RouterModule } from "@angular/router";
 
 const routes = [
     { path: '', component: TestRequestComponent },
-   // { path: 'add', loadChildren: 'app/body/TestRequest/Add/add.module' },
+    { path: 'add', loadChildren: 'app/body/TestRequest/Add/add.module' },
     { path: 'details/:id', loadChildren: 'app/body/TestRequest/Details/details.module' }
    // { path: 'sensor/:id', loadChildren: 'app/body/TestRequest/Sensor/Details/details.module' }
 ];
