bf35c6028cd0e8ec02d8c786242c0a2f97190b5c
Kalyan <kalyan@12thwonder.com>
2016-12-29 12:25:09 -0800

    Step BreadCrumb

--- Titan.WebCore/app/body/Admin/Vehicle/Step/step.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/Step/step.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { StepComponent } from "./step.component";
-import { DataTableModule, MessagesModule, GrowlModule } from 'primeng/primeng';
+import { DataTableModule, MessagesModule, GrowlModule, BreadcrumbModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
+import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
 
 import { StepService } from '../../../../shared/services/step.service';
 
 import stepRoutes from "./step.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, stepRoutes, MessagesModule, GrowlModule],
+    imports: [CommonModule, DataTableModule, GridModule, stepRoutes, MessagesModule, BreadcrumbModule, GrowlModule],
     providers: [StepService],
     declarations: [StepComponent]
 })
