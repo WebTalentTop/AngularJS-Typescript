589d9de6715f3001ef8c1f857f5c0fb87949e7f3
Kalyan <kalyan@12thwonder.com>
2016-12-29 16:31:28 -0800

    TitanRole HomePage BreadCrumb

--- Titan.WebCore/app/body/Admin/Vehicle/TitanRole/titanrole.component.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/TitanRole/titanrole.component.ts
@@ -1,9 +1,10 @@
 import { TitanRoleService } from '../../../../shared/services/titanRole.service';
 import { LoggerService } from '../../../../shared/services/logger.service';
-import { DataTable, LazyLoadEvent, Message, MessagesModule } from 'primeng/primeng';
+import { DataTable, LazyLoadEvent, Message, MessagesModule, MenuItem } from 'primeng/primeng';
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { GridComponent } from '../../../../shared/UIComponents/GridComponent/grid.component'
+import { GridComponent } from '../../../../shared/UIComponents/GridComponent/grid.component';
+import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
 
 @Component({
     selector: 'titanRole-grid',
@@ -17,15 +18,30 @@ export class TitanRoleComponent {
     gridFilter = {};
     msgs: Message[] = [];
     added: any;
-    constructor(private service: TitanRoleService, private route: ActivatedRoute, private router: Router, private logger: LoggerService) {
+    constructor(private breadCrumbsService: BreadCrumbsService,private service: TitanRoleService, private route: ActivatedRoute, private router: Router, private logger: LoggerService) {
 
     }
     
+    breadcrumbs: MenuItem[];
+    breadcrumbsHome: MenuItem;
     ngOnInit() {
     
         this.route.queryParams.subscribe(params => {
 
             this.added = params['page'];
+            let breadC = this.breadCrumbsService.getBreadCrumbs();
+            let titanRoleBreadCrumb = breadC.filter(filter =>
+                filter.pageName === 'TitanRoleHomePage'
+            )[0];
+
+            console.log("BreadC -----", breadC);
+            console.log("titanRoleBreadCrumb ---------", titanRoleBreadCrumb);
+            this.breadcrumbs = [];
+            this.breadcrumbs = titanRoleBreadCrumb.items;
+
+            console.log("breadcurmbs ------", this.breadcrumbs);
+
+            this.breadcrumbsHome = { routerLink: ['/'] };
            
         });
 
