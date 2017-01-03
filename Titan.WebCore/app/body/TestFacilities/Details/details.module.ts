87578aca984b73cd4152f5aa0cee1114913c6c32
Archana <archana@12thwonder.com>
2017-01-02 16:42:08 -0800

    file upload

--- Titan.WebCore/app/body/TestFacilities/Details/details.module.ts
+++ Titan.WebCore/app/body/TestFacilities/Details/details.module.ts
@@ -17,6 +17,7 @@ import { TestTypeService } from '../../../shared/services/testType.service';
 import { TestFacilityAttachmentService } from '../../../shared/services/testFacilityAttachment.service';
 import { FormPreviewModule } from '../../../shared/UIComponents/FormComponents/FormPreviewComponent/formPreview.module';
 import { FormInstanceModule } from '../../../shared/UIComponents/FormComponents/FormInstanceComponent/formInstance.module';
+
 import { BreadCrumbsService } from '../../../shared/services/breadCrumbs/breadCrumbs.service';
 import { DataTableModule, AutoCompleteModule, DataGridModule, TabViewModule, ButtonModule, CalendarModule, 
     CheckboxModule, InputTextareaModule, InputTextModule, PanelModule, DropdownModule, EditorModule, 
@@ -31,6 +32,7 @@ import detailsRoutes from "./details.routes";
 
 @NgModule({
 
+
     imports: [CommonModule, FormPreviewModule, DataGridModule, FormInstanceModule, RouterModule, AutoCompleteModule, 
     CheckboxModule, MultiSelectModule, FormsModule, SpinnerModule, PaginatorModule, EditorModule, DataTableModule, 
     TabViewModule, CalendarModule, ButtonModule, InputTextareaModule, DropdownModule, InputTextModule, PanelModule, 
@@ -39,6 +41,8 @@ import detailsRoutes from "./details.routes";
     providers: [EntityIdentifierService,TestFacilityService,FormSchemaCategoryService, TestFacilityRoleService, 
     TestFacilityAttachmentService, BuildLevelService, ProjectService, TestRoleService, TestStatusService, 
     TestModeService, BreadCrumbsService, TestTypeService, TestTemplateService],
+
+
 
     declarations: [DetailsComponent]
 })
