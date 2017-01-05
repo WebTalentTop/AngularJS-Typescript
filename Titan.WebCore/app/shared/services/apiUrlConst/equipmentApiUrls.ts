0e457cf8d6c009084b6ce777af5dfaffc3f9ae44
Archana <archana@12thwonder.com>
2017-01-04 16:55:35 -0800

    equipment changes

--- Titan.WebCore/app/shared/services/apiUrlConst/equipmentApiUrls.ts
+++ Titan.WebCore/app/shared/services/apiUrlConst/equipmentApiUrls.ts
@@ -6,6 +6,8 @@ export class EquipmentApiUrl {
     public static postCreatedUrl: string = titanApiUrl + 'equipment';
     public static postUpdateUrl: string = titanApiUrl + 'equipment/put';
     public static getByIdUrl: string = titanApiUrl + 'equipment';
+    public static getManufacturerDetailsByIdUrl: string = titanApiUrl + 'equipmentManufacturer';
+    
     public static getAllUrl: string = titanApiUrl + 'equipmentManufacturer';
     public static getAllEquipmentTypesUrl: string = titanApiUrl + 'Equipmenttype/GetAllEquipmentTypes';
    
