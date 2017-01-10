84760c4087637fbc496f05bf33c3e79cd30eda81
Dev <toptal.alltime@gmail.com>
2017-01-10 01:12:36 -0800

    Team Info Updated

--- Titan.WebCore/app/body/Project/TeamInformation/teaminformation.module.ts
+++ Titan.WebCore/app/body/Project/TeamInformation/teaminformation.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { TeamInformationComponent } from './teaminformation.component';
 
 import { TorquesheetService } from './../../../shared/services/torquesheet.service';
-import { DataTableModule, ButtonModule, InputTextareaModule, InputTextModule, PanelModule, DropdownModule,
+import { AutoCompleteModule, CheckboxModule, DataTableModule, ButtonModule, InputTextareaModule, InputTextModule, PanelModule, DropdownModule,
     TreeTableModule,TreeNode,SharedModule, DialogModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 
 
 @NgModule({
-    imports: [CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule,
+    imports: [AutoCompleteModule, CheckboxModule, CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule,
         ButtonModule, DropdownModule,TreeTableModule,SharedModule,DialogModule],
     providers: [TorquesheetService],
     declarations: [TeamInformationComponent],
