719b7a7473bd54ba3903b2fa771de138d1e2d854
Dev <toptal.alltime@gmail.com>
2017-01-13 08:34:59 -0800

    Test Template Add

--- Titan.WebCore/app/body/TestTemplate/Add/add.module.ts
+++ Titan.WebCore/app/body/TestTemplate/Add/add.module.ts
@@ -6,12 +6,12 @@ import { TestTypeService} from './../../../shared/services/testtype.service';
 import { TestModeService } from './../../../shared/services/testmode.service';
 import { TestTemplateService} from './../../../shared/services/testtemplate.service';
 
-import { DataTableModule, ButtonModule, InputTextareaModule,InputTextModule, PanelModule, DropdownModule } from 'primeng/primeng';
+import { PanelModule, DataTableModule, ButtonModule, InputTextareaModule,InputTextModule, PanelModule, DropdownModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import addRoutes from "./add.routes";
 
 @NgModule({
-    imports: [CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, ButtonModule,DropdownModule, addRoutes],
+    imports: [PanelModule, CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, ButtonModule,DropdownModule, addRoutes],
     providers: [TestTemplateService, TestTypeService, TestModeService ],
     declarations: [AddComponent]
 })
