5d2f5e7fa101d6b35cb3e194a064317f65a2b90a
Dev <toptal.alltime@gmail.com>
2017-01-14 01:44:36 -0800

    TT Grid

--- Titan.WebCore/app/body/TestTemplate/testtemplate.module.ts
+++ Titan.WebCore/app/body/TestTemplate/testtemplate.module.ts
@@ -2,13 +2,13 @@
 import { CommonModule } from "@angular/common";
 import { TestTemplateComponent } from "./testtemplate.component";
 import { TestTemplateService } from '../../shared/services/testTemplate.service';
-import { DataTableModule, TabViewModule, AutoCompleteModule, InputTextModule, InputTextareaModule, DropdownModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, TabViewModule, AutoCompleteModule, InputTextModule, InputTextareaModule, DropdownModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule } from '../../shared/UIComponents/GridComponent/grid.module';
 import testTemplateRoutes from "./testtemplate.routes";
 
 @NgModule({
-    imports: [CommonModule, RouterModule, DataTableModule, InputTextModule, DropdownModule,
+    imports: [ButtonModule, CommonModule, RouterModule, DataTableModule, InputTextModule, DropdownModule,
         InputTextareaModule, TabViewModule, GridModule, testTemplateRoutes, AutoCompleteModule],
     providers: [TestTemplateService],
     declarations: [TestTemplateComponent]
