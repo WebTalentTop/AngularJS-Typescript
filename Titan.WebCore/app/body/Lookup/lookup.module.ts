dababcfdac649bc007897d87d20744d5e40d03b3
Dev <toptal.alltime@gmail.com>
2017-01-19 11:00:31 -0800

    Step & Admin & Lookup

--- Titan.WebCore/app/body/Lookup/lookup.module.ts
+++ Titan.WebCore/app/body/Lookup/lookup.module.ts
@@ -2,12 +2,12 @@
 import { CommonModule } from "@angular/common";
 import { LookupComponent } from "./lookup.component";
 import { LookupService } from '../../shared/services/lookup.service';
-import { DataTableModule } from 'primeng/primeng';
+import { DataTableModule, BreadcrumbModule, AccordionModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import lookupRoutes from "./lookup.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, lookupRoutes],
+    imports: [CommonModule, DataTableModule,BreadcrumbModule, AccordionModule, lookupRoutes],
     providers: [LookupService],
     declarations: [LookupComponent]
 })
