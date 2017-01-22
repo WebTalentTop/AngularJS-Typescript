68473fd4ab0aecb8c0568af4e0464b670e1bf582
Kalyan <kalyan@12thwonder.com>
2017-01-21 16:09:34 -0800

    TestTemplate Homepage BreadCrumbs

--- Titan.WebCore/app/body/TestTemplate/testtemplate.module.ts
+++ Titan.WebCore/app/body/TestTemplate/testtemplate.module.ts
@@ -2,15 +2,16 @@
 import { CommonModule } from "@angular/common";
 import { TestTemplateComponent } from "./testtemplate.component";
 import { TestTemplateService } from '../../shared/services/Containers/TestTemplateService/testTemplate.service';
-import { ButtonModule, DataTableModule, TabViewModule, AutoCompleteModule, InputTextModule, InputTextareaModule, DropdownModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, TabViewModule, AutoCompleteModule,BreadcrumbModule, InputTextModule, InputTextareaModule, DropdownModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule } from '../../shared/UIComponents/GridComponent/grid.module';
 import testTemplateRoutes from "./testtemplate.routes";
+import { BreadCrumbsService } from '../../shared/services/breadCrumbs/breadCrumbs.service';
 
 @NgModule({
-    imports: [ButtonModule, CommonModule, RouterModule, DataTableModule, InputTextModule, DropdownModule,
+    imports: [ButtonModule, CommonModule, RouterModule, DataTableModule, InputTextModule,BreadcrumbModule, DropdownModule,
         InputTextareaModule, TabViewModule, GridModule, testTemplateRoutes, AutoCompleteModule],
-    providers: [TestTemplateService],
+    providers: [TestTemplateService, BreadCrumbsService],
     declarations: [TestTemplateComponent]
 })
 
