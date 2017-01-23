72cb71a6d110e15635acb279a251eacd9f51ba1a
Dev <toptal.alltime@gmail.com>
2017-01-23 03:34:35 -0800

    Milestone Type Pages

--- Titan.WebCore/app/body/Admin/Vehicle/MilestoneType/Add/add.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/MilestoneType/Add/add.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AddComponent } from "./add.component";
 import { MilestoneTypeService} from '../../../../../shared/services/milestoneType.service';
 
-import { DataTableModule, ButtonModule, InputTextareaModule,InputTextModule, PanelModule, DropdownModule } from 'primeng/primeng';
+import { BreadcrumbModule, DataTableModule, ButtonModule, InputTextareaModule,InputTextModule, PanelModule, DropdownModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import addRoutes from "./add.routes";
 
 @NgModule({
-    imports: [CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, ButtonModule,DropdownModule, addRoutes],
+    imports: [BreadcrumbModule, CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, ButtonModule,DropdownModule, addRoutes],
     providers: [MilestoneTypeService],
     declarations: [AddComponent]
 })
