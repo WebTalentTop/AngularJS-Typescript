009132f0c37acc75ac7134216aa8fd39df65c77c
Dev <toptal.alltime@gmail.com>
2017-01-24 11:41:33 -0800

    Maintenance Frequency

--- Titan.WebCore/app/body/Admin/Vehicle/MaintenanceFrequency/maintenanceFrequency.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/MaintenanceFrequency/maintenanceFrequency.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MaintenanceFrequencyComponent } from "./maintenanceFrequency.component";
-import { DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
 import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
@@ -10,7 +10,7 @@ import { MaintenanceFrequencyService } from '../../../../shared/services/mainten
 import maintenanceFrequencyRoutes from "./maintenanceFrequency.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, maintenanceFrequencyRoutes,BreadcrumbModule, MessagesModule, GrowlModule],
+    imports: [ButtonModule, CommonModule, DataTableModule, GridModule, maintenanceFrequencyRoutes,BreadcrumbModule, MessagesModule, GrowlModule],
     providers: [MaintenanceFrequencyService,BreadCrumbsService],
     declarations: [MaintenanceFrequencyComponent]
 })
