65bb15900002a76ab877e4a022233a2e31356253
Dev <toptal.alltime@gmail.com>
2017-01-24 11:24:15 -0800

    Operating Hours

--- Titan.WebCore/app/body/Admin/Vehicle/OperatingHours/Add/add.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/OperatingHours/Add/add.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AddComponent } from "./add.component";
 import { OperatingHoursService} from '../../../../../shared/services/operatingHours.service';
 
-import { DataTableModule, ButtonModule, InputTextareaModule, InputTextModule, CalendarModule, PanelModule, DropdownModule } from 'primeng/primeng';
+import { EditorModule, SharedModule, BreadcrumbModule, DataTableModule, ButtonModule, InputTextareaModule, InputTextModule, CalendarModule, PanelModule, DropdownModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import addRoutes from "./add.routes";
 
 @NgModule({
-    imports: [CommonModule, FormsModule, DataTableModule, InputTextareaModule, CalendarModule, InputTextModule, PanelModule, ButtonModule, DropdownModule, addRoutes],
+    imports: [EditorModule, SharedModule, BreadcrumbModule, CommonModule, FormsModule, DataTableModule, InputTextareaModule, CalendarModule, InputTextModule, PanelModule, ButtonModule, DropdownModule, addRoutes],
     providers: [OperatingHoursService],
     declarations: [AddComponent]
 })
--- Titan.WebCore/app/body/Admin/Vehicle/OperatingHours/operatingHours.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/OperatingHours/operatingHours.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { OperatingHoursComponent } from "./operatingHours.component";
-import { DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
 import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
@@ -10,7 +10,7 @@ import { OperatingHoursService } from '../../../../shared/services/operatingHour
 import operatingHoursRoutes from "./operatingHours.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, operatingHoursRoutes, MessagesModule, GrowlModule,BreadcrumbModule],
+    imports: [ButtonModule, CommonModule, DataTableModule, GridModule, operatingHoursRoutes, MessagesModule, GrowlModule,BreadcrumbModule],
     providers: [OperatingHoursService,BreadCrumbsService],
     declarations: [OperatingHoursComponent]
 })
