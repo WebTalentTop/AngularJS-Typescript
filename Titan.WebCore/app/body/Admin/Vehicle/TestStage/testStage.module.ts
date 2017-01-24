b71c883bd6b96091d6be4879c06f86e9cf080c43
Dev <toptal.alltime@gmail.com>
2017-01-24 10:41:25 -0800

    Test Stage

--- Titan.WebCore/app/body/Admin/Vehicle/TestStage/testStage.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/TestStage/testStage.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TestStageComponent } from "./testStage.component";
-import { DataTableModule, MessagesModule, GrowlModule ,BreadcrumbModule} from 'primeng/primeng';
+import { ButtonModule, DataTableModule, MessagesModule, GrowlModule ,BreadcrumbModule} from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
 import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
@@ -10,7 +10,7 @@ import { TestStageService } from '../../../../shared/services/testStage.service'
 import testStageRoutes from "./testStage.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, testStageRoutes, MessagesModule, BreadcrumbModule,GrowlModule],
+    imports: [ButtonModule, CommonModule, DataTableModule, GridModule, testStageRoutes, MessagesModule, BreadcrumbModule,GrowlModule],
     providers: [TestStageService,BreadCrumbsService],
     declarations: [TestStageComponent]
 })
