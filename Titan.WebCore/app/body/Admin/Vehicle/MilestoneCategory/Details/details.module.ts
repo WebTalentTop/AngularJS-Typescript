3f62366c3f98057dcc0ad1dde9f3f9f399f7e9ea
Kalyan <kalyan@12thwonder.com>
2017-01-24 16:11:39 -0800

    MilestoneCategory BreadCrumbs Add/Details

--- Titan.WebCore/app/body/Admin/Vehicle/MilestoneCategory/Details/details.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/MilestoneCategory/Details/details.module.ts
@@ -2,8 +2,8 @@
 import { CommonModule } from "@angular/common";
 import { FormsModule } from '@angular/forms';
 import { DetailsComponent } from "./details.component";
-import { ProjectStatusService} from '../../../../../shared/services/projectStatus.service';
-
+import { MilestoneCategoryService} from '../../../../../shared/services/milestoneCategory.service';
+import { BreadCrumbsService } from '../../../../../shared/services/breadCrumbs/breadCrumbs.service';
 import { BreadcrumbModule, DataTableModule, ButtonModule, InputTextareaModule, ToggleButtonModule, InputTextModule, PanelModule, DropdownModule, CalendarModule, GrowlModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import detailsRoutes from "./details.routes";
@@ -11,7 +11,7 @@ import detailsRoutes from "./details.routes";
 @NgModule({
     imports: [BreadcrumbModule, CommonModule, FormsModule, DataTableModule, InputTextareaModule, ToggleButtonModule, InputTextModule, PanelModule,
         ButtonModule, DropdownModule, CalendarModule, GrowlModule, detailsRoutes],
-    providers: [ProjectStatusService],
+    providers: [MilestoneCategoryService, BreadCrumbsService],
     declarations: [DetailsComponent],
     exports: [DetailsComponent]
 })
