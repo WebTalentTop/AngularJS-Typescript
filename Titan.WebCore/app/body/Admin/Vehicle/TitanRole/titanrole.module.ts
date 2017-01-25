a7f35cc8e40a4e3238c6fc6254697017d970d88f
Dev <toptal.alltime@gmail.com>
2017-01-25 02:16:55 -0800

    Titan Role Page

--- Titan.WebCore/app/body/Admin/Vehicle/TitanRole/titanrole.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/TitanRole/titanrole.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TitanRoleComponent } from "./titanRole.component";
-import { DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
 import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
@@ -10,7 +10,7 @@ import { TitanRoleService } from '../../../../shared/services/titanRole.service'
 import titanRoleRoutes from "./titanRole.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, titanRoleRoutes, MessagesModule, GrowlModule,BreadcrumbModule],
+    imports: [ButtonModule, CommonModule, DataTableModule, GridModule, titanRoleRoutes, MessagesModule, GrowlModule,BreadcrumbModule],
     providers: [TitanRoleService,BreadCrumbsService],
     declarations: [TitanRoleComponent]
 })
