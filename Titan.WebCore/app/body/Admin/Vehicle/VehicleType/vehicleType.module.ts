6da4055ddc10f998ed94f4bac61da2c2140c65f0
Dev <toptal.alltime@gmail.com>
2017-01-25 01:53:39 -0800

    Vehicle Type

--- Titan.WebCore/app/body/Admin/Vehicle/VehicleType/vehicleType.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/VehicleType/vehicleType.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { VehicleTypeComponent } from "./vehicleType.component";
-import { DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
 import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
@@ -10,7 +10,7 @@ import { VehicleTypeService } from '../../../../shared/services/vehicleType.serv
 import vehicleTypeRoutes from "./vehicleType.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, vehicleTypeRoutes, MessagesModule,BreadcrumbModule, GrowlModule],
+    imports: [ButtonModule, CommonModule, DataTableModule, GridModule, vehicleTypeRoutes, MessagesModule,BreadcrumbModule, GrowlModule],
     providers: [VehicleTypeService,BreadCrumbsService],
     declarations: [VehicleTypeComponent]
 })
