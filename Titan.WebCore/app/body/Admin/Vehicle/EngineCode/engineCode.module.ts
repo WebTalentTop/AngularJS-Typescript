53bb1d6cd7373402f84bd6749d86c0a06c9e2baf
Dev <toptal.alltime@gmail.com>
2017-01-25 01:47:56 -0800

    Engine Code page

--- Titan.WebCore/app/body/Admin/Vehicle/EngineCode/engineCode.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/EngineCode/engineCode.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { EngineCodeComponent } from "./engineCode.component";
-import { DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
 import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
@@ -10,7 +10,7 @@ import { EngineCodeService } from '../../../../shared/services/engineCode.servic
 import engineCodeRoutes from "./engineCode.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, engineCodeRoutes, MessagesModule, GrowlModule,BreadcrumbModule],
+    imports: [ButtonModule, CommonModule, DataTableModule, GridModule, engineCodeRoutes, MessagesModule, GrowlModule,BreadcrumbModule],
     providers: [EngineCodeService,BreadCrumbsService],
     declarations: [EngineCodeComponent]
 })
