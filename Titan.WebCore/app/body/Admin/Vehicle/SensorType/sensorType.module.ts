eab0ceb3cbd97e7d1f06badec21d89deb06a3699
Dev <toptal.alltime@gmail.com>
2017-01-25 02:05:30 -0800

    Sensor Type

--- Titan.WebCore/app/body/Admin/Vehicle/SensorType/sensorType.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/SensorType/sensorType.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SensorTypeComponent } from "./sensorType.component";
-import { DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
+import { ButtonModule, DataTableModule, MessagesModule, GrowlModule,BreadcrumbModule } from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import { GridModule} from '../../../../shared/UIComponents/GridComponent/grid.module';
 import { BreadCrumbsService } from '../../../../shared/services/breadCrumbs/breadCrumbs.service';
@@ -10,7 +10,7 @@ import { SensorTypeService } from '../../../../shared/services/sensorType.servic
 import sensorTypeRoutes from "./sensorType.routes";
 
 @NgModule({
-    imports: [CommonModule, DataTableModule, GridModule, sensorTypeRoutes,BreadcrumbModule, MessagesModule, GrowlModule],
+    imports: [ButtonModule, CommonModule, DataTableModule, GridModule, sensorTypeRoutes,BreadcrumbModule, MessagesModule, GrowlModule],
     providers: [SensorTypeService,BreadCrumbsService],
     declarations: [SensorTypeComponent]
 })
