818cf3a2a628d5eb618fe3e562a87226a8e6a065
ajay12thwonder <ajay@12thwonder.com>
2017-01-24 21:28:48 -0800

    updating the routes from testrequest or testRequest

--- Titan.WebCore/app/body/FunctionGroup/functionGroup.routes.ts
+++ Titan.WebCore/app/body/FunctionGroup/functionGroup.routes.ts
@@ -1,12 +1,12 @@
-﻿import { FunctionGroupComponent } from "./functionGroup.component";
-import { RouterModule } from "@angular/router";
+﻿import {FunctionGroupComponent} from "./functionGroup.component";
+import {RouterModule} from "@angular/router";
 import {AuthGuard} from "../../shared/services/auth/authGuard";
 
 const routes = [
-    { path: '', component: FunctionGroupComponent },
-    { path: 'details/:id', loadChildren: 'app/body/FunctionGroup/Details/details.module' },
-    { path: 'add', loadChildren: 'app/body/FunctionGroup/Add/add.module' },
-  //  { path: 'testrequest/details/:testRequestId/add/:testrequestId/:taskId', canActivate: [AuthGuard], loadChildren: 'app/body/TestRequest/Sensor/add.module' },
+    {path: '', component: FunctionGroupComponent},
+    {path: 'details/:id', loadChildren: 'app/body/FunctionGroup/Details/details.module'},
+    {path: 'add', loadChildren: 'app/body/FunctionGroup/Add/add.module'},
+    //  { path: 'testRequest/details/:testRequestId/add/:testRequestId/:taskId', canActivate: [AuthGuard], loadChildren: 'app/body/TestRequest/Sensor/add.module' },
     //{ path: 'details/:taskId', canActivate: [AuthGuard], loadChildren: 'app/body/Task/Details/details.module' }
 
 ];
