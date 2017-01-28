221aca1d007cad24dbad697f91f2598db4c83d4f
ZeroInfinity12th <atilla@12thWonder.com>
2017-01-28 01:54:17 -0800

    Adding Attachment Workflow to Add and Details to FormBuilder

--- Titan.WebCore/app/body/Admin/FormBuilder/details/details.module.ts
+++ Titan.WebCore/app/body/Admin/FormBuilder/details/details.module.ts
@@ -8,7 +8,21 @@ import {CommonModule} from "@angular/common";
 import { FormsModule} from '@angular/forms';
 import {DetailsComponent} from "./details.component";
 import detailsRoute from './details.routes';
-import { PanelModule,DataTableModule, MessagesModule, DragDropModule, DialogModule, InputTextModule, SpinnerModule, DropdownModule, RadioButtonModule, ButtonModule, CheckboxModule, CalendarModule } from 'primeng/primeng';
+import {
+    PanelModule,
+    DataTableModule,
+    MessagesModule,
+    DragDropModule,
+    DialogModule,
+    InputTextModule,
+    SpinnerModule,
+    DropdownModule,
+    RadioButtonModule,
+    ButtonModule,
+    CheckboxModule,
+    ToggleButtonModule,
+    CalendarModule
+} from 'primeng/primeng';
 
 
 @NgModule({
@@ -29,6 +43,7 @@ import { PanelModule,DataTableModule, MessagesModule, DragDropModule, DialogModu
         CalendarModule,
         MessagesModule,
         PanelModule,
+        ToggleButtonModule,
         detailsRoute
     ],
     declarations:[DetailsComponent]
