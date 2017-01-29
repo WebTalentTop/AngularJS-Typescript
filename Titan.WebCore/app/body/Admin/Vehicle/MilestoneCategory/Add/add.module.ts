1f467261c693bfc682dedd14cd40f8432de3e7a9
Dev <toptal.alltime@gmail.com>
2017-01-29 04:49:27 -0800

    MC Msg

--- Titan.WebCore/app/body/Admin/Vehicle/MilestoneCategory/Add/add.module.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/MilestoneCategory/Add/add.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AddComponent } from "./add.component";
 import { MilestoneCategoryService} from '../../../../../shared/services/milestoneCategory.service';
 import { BreadCrumbsService } from '../../../../../shared/services/breadCrumbs/breadCrumbs.service';
-import { DataTableModule, ButtonModule, InputTextareaModule,InputTextModule, PanelModule, DropdownModule , BreadcrumbModule} from 'primeng/primeng';
+import { GrowlModule,DataTableModule, ButtonModule, InputTextareaModule,InputTextModule, PanelModule, DropdownModule , BreadcrumbModule} from 'primeng/primeng';
 import { RouterModule } from "@angular/router";
 import addRoutes from "./add.routes";
 
 @NgModule({
-    imports: [BreadcrumbModule, CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, ButtonModule,DropdownModule, addRoutes],
+    imports: [GrowlModule,BreadcrumbModule, CommonModule, FormsModule, DataTableModule, InputTextareaModule, InputTextModule, PanelModule, ButtonModule,DropdownModule, addRoutes],
     providers: [MilestoneCategoryService, BreadCrumbsService],
     declarations: [AddComponent]
 })
