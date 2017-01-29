ba6a25a3f5fb525ee471555563bc436b93c11a66
Dev <toptal.alltime@gmail.com>
2017-01-29 04:57:08 -0800

    PR, PS Msg

--- Titan.WebCore/app/body/Admin/Vehicle/ProjectStatus/Add/add.component.ts
+++ Titan.WebCore/app/body/Admin/Vehicle/ProjectStatus/Add/add.component.ts
@@ -63,8 +63,9 @@ export class AddComponent {
             // this.router.navigate(["/vehicle/projectStatus/", res]);
             if (res.isSuccess) {
                 //this.router.navigate([], {q})
-                this.router.navigate(["/admin/vehicle/projectStatus"], { queryParams: { page: 1 } });
-               
+                this.msgs = [];
+                this.msgs.push({ severity: 'success', summary: 'Success', detail: '' });
+                setTimeout(()=>this.router.navigate(["admin/vehicle/projectStatus"], { queryParams: { page: 1 } }),2000);
             }
            
         }
